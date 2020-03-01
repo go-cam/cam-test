540035b85588449f66d3b93cae805451178feb38
Cinling <1105412681@qq.com>
2020-03-01 13:54:05 +0800

    add: LogComponent log level

--- backend/controllers/TestController.go
+++ backend/controllers/TestController.go
@@ -11,13 +11,18 @@ type TestController struct {
 
 func (controller *TestController) Init() {
 	controller.Controller.Init()
+}
 
-	controller.SetDefaultActionName("test")
+func (controller *TestController) GetDefaultActionName() string {
+	return "Test"
 }
 
 // test action
 func (controller *TestController) Test() {
+	cam.App.Debug("title", "content")
 	cam.App.Info("title", "content")
+	cam.App.Warn("title", "content")
+	cam.App.Error("title", "content")
 	controller.SetResponse([]byte("done"))
 }
 
